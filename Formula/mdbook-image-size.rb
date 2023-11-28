ae5498d789551cb75dab0c815e2746f5164bd2f8
lhybdv <itelite39660787@gmail.com>
2023-11-28 22:28:10 +0800

    set branch to main

---
+++ Formula/mdbook-image-size.rb
@@ -0,0 +1,14 @@
+class MdbookImageSize < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/lhybdv/mdbook-image-size.git", branch: main
+  version "0.1.0"
+  sha256 ""
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+end
